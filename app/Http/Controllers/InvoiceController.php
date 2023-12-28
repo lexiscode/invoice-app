226b9a7bab3717f3aa100255392f62c552389c58
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-28 21:54:05 +0100

    feat: setup reading from the db into the invoice index page

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function index()
+    {
+        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,4 @@ import app from './components/app.vue';
 import router from './router/index.js';
 
 createApp(app).use(router).mount('#app');
+
